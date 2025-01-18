29c70d0c660df3c202ce6918274a1c0afcfca7f4
Martin SangDo <martin.sangdo@gmail.com>
2025-01-18 23:08:38 +0000

    - change height of the widget

--- api/stock.js
+++ api/stock.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 //http://localhost:3000/stock/chart
+//https://nodejs-stock-forum.onrender.com/stock/chart
 router.get('/chart', function(req, res, next) {
     res.render('chart');
 });
